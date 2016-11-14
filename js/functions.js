7da4ce49dca12ac6aadb5ec4ea6a90288e79cc83
Lisa Hull <lisa@jchull.com>
2016-11-14 12:56:47 -0700

    first commit :sparkles:

---
+++ js/functions.js
@@ -0,0 +1,63 @@
+$(window).scroll(function(){
+
+  var wScroll = $(this).scrollTop();
+
+  $('.logo').css({
+    'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+  });
+
+  $('.back-bird').css({
+    'transform' : 'translate(0px, '+ wScroll /4 +'%)'
+  });
+
+  $('.fore-bird').css({
+    'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
+  });
+
+  if(wScroll > $('.picasa').offset().top - ($(window).height() / 1.2)) {
+
+    $('.picasa figure').each(function(i){
+
+      setTimeout(function(){
+        $('.picasa figure').eq(i).addClass('is-showing');
+      }, 150 * (i+1));
+    });
+
+  }
+
+  if(wScroll > $('.large-window').offset().top - $(window).height()){
+
+    $('.large-window').css({'background-position':'center '+ (wScroll - $('.large-window').offset().top) +'px'});
+
+    var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 50);
+
+    $('.window-tint').css({'opacity': opacity});
+
+  }
+
+  if(wScroll > $('.blog-posts').offset().top - $(window).height()){
+
+    var offset = Math.min(0, wScroll - $('.blog-posts').offset().top +$(window).height() - 350);
+
+    $('.post-1').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.2) +'px)'});
+
+    $('.post-3').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.2) +'px)'});
+
+  }
+
+});
+
+$(document).ready(function(){
+  var mouseX, mouseY;
+  var ww = $( window ).width();
+  var wh = $( window ).height();
+  var traX, traY;
+  $(document).mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((4 * mouseX) / 570) + 40;
+    traY = ((4 * mouseY) / 570) + 50;
+    console.log(traX);
+    $(".title").css({"background-position": traX + "%" + traY + "%"});
+  });
+});
---
+++ readme.md
@@ -0,0 +1,15 @@
+
+
+[Best Parallax Tutorial on the Web](https://github.com/DevTips/Parallax-on-the-Web-DevTips-) by [@DevTipsShow](https://twitter.com/devtipsshow)
+
+Parallax Star Background CSS on CodePen http://codepen.io/saransh/pen/BKJun
+
+moving background-image on mousemove http://codepen.io/chrisboon27/pen/rEDIC by http://codepen.io/chrisboon27/
+
+Parallax backgrounds with walk cycle http://codepen.io/rachelnabors/pen/AvGhp by http://codepen.io/rachelnabors/
+
+Floating Cloud background http://codepen.io/shshaw/pen/DxJka by http://codepen.io/shshaw/ 
+
+Peek Through the Background http://codepen.io/timteeling/pen/KDlCd by http://codepen.io/timteeling/
+
+
